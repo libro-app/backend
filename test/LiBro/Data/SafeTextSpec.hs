e2ef504ff93651404bdb18ee266f47d441f9bb10
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-08-18 11:33:57 +0200

    Enhance density and readability in SafeText tests

--- test/LiBro/Data/SafeTextSpec.hs
+++ test/LiBro/Data/SafeTextSpec.hs
@@ -36,22 +36,22 @@ safetyChecks = describe "Safety checks" $ do
   it "Correct unsafe characters" $
     unsafeChars `shouldBe` "\NUL\r"
 
-  prop "Check safe characters" $
-    \c -> safe [c] ==> isSafeChar c
+  prop "Check safe characters" $ \c ->
+    safe [c] ==> isSafeChar c
 
-  prop "Check unsafe characters" $
-    \c -> unsafe [c] ==> not (isSafeChar c)
+  prop "Check unsafe characters" $ \c ->
+    unsafe [c] ==> not (isSafeChar c)
 
-  prop "Check safe strings" $
-    \s -> safe s ==> isSafeString s
+  prop "Check safe strings" $ \s ->
+    safe s ==> isSafeString s
 
-  prop "Check unsafe strings" $
-    \s -> unsafe s ==> not (isSafeString s)
+  prop "Check unsafe strings" $ \s ->
+    unsafe s ==> not (isSafeString s)
 
-  prop "Check Text" $
-    \t -> classify (safe (T.unpack t)) "safe" $
-          classify (unsafe (T.unpack t)) "unsafe" $
-          isSafeText t `shouldBe` isSafeString (T.unpack t)
+  prop "Check Text" $ \t ->
+    classify (safe (T.unpack t)) "safe" $
+    classify (unsafe (T.unpack t)) "unsafe" $
+    isSafeText t `shouldBe` isSafeString (T.unpack t)
 
   where safe    = all (`notElem` unsafeChars)
         unsafe  = any (`elem` unsafeChars)
@@ -59,20 +59,21 @@ safetyChecks = describe "Safety checks" $ do
 safePacking :: Spec
 safePacking = describe "Safe packing" $ do
 
-  prop "Pack safe Text" $
-    \t -> isSafeText t ==> getText <$> safePackText t `shouldBe`  Just t
+  prop "Pack safe Text" $ \t ->
+    isSafeText t ==> getText <$> safePackText t `shouldBe`  Just t
 
-  prop "Pack unsafe Text" $
-    \t -> not (isSafeText t) ==> safePackText t `shouldBe` Nothing
+  prop "Pack unsafe Text" $ \t ->
+    not (isSafeText t) ==> safePackText t `shouldBe` Nothing
 
-  prop "Check String input" $
-    \s -> safePack s `shouldBe` safePackText (T.pack s)
+  prop "Check String input" $ \s ->
+    safePack s `shouldBe` safePackText (T.pack s)
 
 showInstance :: Spec
 showInstance = describe "Show instance" $ do
-  prop "Same result as Text on safe input" $ \t -> isSafeText t ==>
-      let (Just st) = safePackText t
-      in  show st `shouldBe` show t
+  prop "Same result as Text on safe input" $ \t ->
+    isSafeText t ==>
+    let (Just st) = safePackText t
+    in  show st `shouldBe` show t
 
 isStringInstance :: Spec
 isStringInstance = describe "IsString instance (OverloadedStrings)" $ do
@@ -83,8 +84,8 @@ isStringInstance = describe "IsString instance (OverloadedStrings)" $ do
       evaluate (fromString testStr :: SafeText)
         `shouldThrow` errorCall ("Not a safe string: " ++ show testStr)
 
-  prop "No error without unsafe characters" $
-    \s -> isSafeString s ==> getText (fromString s) `shouldBe` T.pack s
+  prop "No error without unsafe characters" $ \s ->
+    isSafeString s ==> getText (fromString s) `shouldBe` T.pack s
 
 arbitraryInstance :: Spec
 arbitraryInstance = describe "Arbitrary instance (QuickCheck)" $ do
@@ -98,47 +99,48 @@ jsonInstances :: Spec
 jsonInstances = describe "JSON instances" $ do
 
   describe "Roundtrip FromJSON . ToJSON = id" $ do
-    prop "Correct SafeText" $
-      \st -> J.decode (J.encode (st :: SafeText)) `shouldBe` Just st
+    prop "Correct SafeText" $ \st ->
+      J.decode (J.encode (st :: SafeText)) `shouldBe` Just st
 
   describe "SafeText accepts valid string input only" $ do
-    prop "Parsing gives Nothing" $
-      \s -> not (isSafeString s) ==>
-        (J.decode (J.encode s) :: Maybe SafeText) `shouldBe` Nothing
+    prop "Parsing gives Nothing" $ \s ->
+      not (isSafeString s) ==>
+      (J.decode (J.encode s) :: Maybe SafeText) `shouldBe` Nothing
 
   describe "JSON roundtrip is equivalent to calling safePack" $
-    prop "They're the same picture" $
-      \s -> classify (isSafeString s) "safe" $
-            classify (not $ isSafeString s) "unsafe" $
-            J.decode (J.encode s) `shouldBe` safePack s
+    prop "They're the same picture" $ \s ->
+      classify (isSafeString s) "safe" $
+      classify (not $ isSafeString s) "unsafe" $
+      J.decode (J.encode s) `shouldBe` safePack s
 
 csvInstances :: Spec
 csvInstances = describe "CSV instances" $ do
 
   describe "Roundtrip FromField . ToField = id" $ do
-    prop "Correct (non-empty) SafeText" $
-      \st -> not (T.null $ getText st) ==>
-        readCSV (writeCSV st) `shouldBe` Right (V.fromList [Only st])
+    prop "Correct (non-empty) SafeText" $ \st ->
+      not (T.null $ getText st) ==>
+      readCSV (writeCSV st) `shouldBe` Right (V.fromList [Only st])
 
   describe "SafeText accepts valid string input only" $ do
-    prop "Parsing gives error message" $
-      \s -> not (isSafeString s) ==>
-        let (Left e) = readCSV (writeCSV' s)
-            expected = "Unsafe string: " ++ show s
-        in  e `shouldContain` expected
+    prop "Parsing gives error message" $ \s ->
+      not (isSafeString s) ==>
+      let (Left e) = readCSV (writeCSV' s)
+          expected = "Unsafe string: " ++ show s
+      in  e `shouldContain` expected
 
   describe "CSV roundtrip is somehow equivalent to calling safePack" $
-    prop "They're the same picture" $
-      \s -> not (null s) ==>
-              classify (isSafeString s) "safe" $
-              classify (not $ isSafeString s) "unsafe" $
-              eitherToMaybe (readCSV (writeCSV' s))
-                `shouldBe` (V.fromList . return . Only <$> safePack s)
+    prop "They're the same picture" $ \s ->
+      not (null s) ==>
+      classify (isSafeString s) "safe" $
+      classify (not $ isSafeString s) "unsafe" $
+      eitherToMaybe (readCSV (writeCSV' s))
+        `shouldBe` (V.fromList . return . Only <$> safePack s)
 
-  where -- explicit types to make tests more readable
-        writeCSV :: SafeText -> ByteString
-        writeCSV = C.encode . return . Only
-        writeCSV' :: String -> ByteString
-        writeCSV' = C.encode . return . Only
-        readCSV :: ByteString -> Either String (Vector (Only SafeText))
-        readCSV = C.decode NoHeader
+  where
+    -- explicit types to make tests more readable
+    writeCSV :: SafeText -> ByteString
+    writeCSV = C.encode . return . Only
+    writeCSV' :: String -> ByteString
+    writeCSV' = C.encode . return . Only
+    readCSV :: ByteString -> Either String (Vector (Only SafeText))
+    readCSV = C.decode NoHeader
