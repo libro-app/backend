8faac1b6c1696fa22cecb092caf8bee3a90138d5
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-09-13 15:30:33 +0200

    Test all no-hello endpoints using QuickCheck

--- test/LiBro/WebServiceSpec.hs
+++ test/LiBro/WebServiceSpec.hs
@@ -2,8 +2,10 @@ module LiBro.WebServiceSpec where
 
 import Test.Hspec
 import Test.Hspec.Wai
+import Test.Hspec.Wai.QuickCheck
 
 import LiBro.WebService.Server
+import Data.ByteString
 
 spec :: Spec
 spec = describe "RESTful JSON web service" $ do
@@ -12,10 +14,12 @@ spec = describe "RESTful JSON web service" $ do
 helloLibro :: Spec
 helloLibro = describe "Dummy: hello libro!" $ with (return libro) $ do
 
-  describe "Root" $ do
-    it "Respond with 404" $ do
-      get "/" `shouldRespondWith` 404
-
   describe "Hello endpoint" $ do
     it "Respond with 200 greeting" $ do
       get "/hello" `shouldRespondWith` "Hello LiBro!" {matchStatus = 200}
+
+  describe "Any other endpoint" $ do
+    it "Respond with 404" $ do
+      property $ \endpoint ->
+        show endpoint /= "hello" ==>
+        get (pack endpoint) `shouldRespondWith` 404
