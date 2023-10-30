ed2ec33e83a70ec0ffbf6dbf1e7515e04878eea7
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-10-30 15:32:14 +0100

    Add xlsx import and export as utility functions

--- lib/LiBro/Util.hs
+++ lib/LiBro/Util.hs
@@ -2,14 +2,24 @@
 --    in more than one place.
 module LiBro.Util where
 
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import Data.Tuple
 import Data.List as L
 import qualified Data.Map as M
+import qualified Data.HashMap.Lazy as HM
+import qualified Data.Vector as V
 import Data.Tree
 import Data.Maybe
+import Data.Csv
 import Data.Bifunctor
 import Control.Monad.State
 import Control.Applicative
+import System.FilePath
+import System.Directory
+import System.IO.Temp
+import System.Process
 
 -- |  A 'Tree'/'Forest' representation as a linear list.
 --    All entries point to their parent.
@@ -44,3 +54,49 @@ runCountingT = evalStateT
 -- |  Create an 'Alternative' value based on a predicate.
 guarded :: Alternative f => (a -> Bool) -> a -> f a
 guarded p x = if p x then pure x else empty
+
+--  Internal newtype wrapper to mask special cell values
+--  like references ("=foo") by prepending '%'
+newtype Wrap a = Wrap {unWrap :: a} deriving Show
+instance ToNamedRecord a => ToNamedRecord (Wrap a) where
+  toNamedRecord = post . toNamedRecord . unWrap
+    where post  = HM.fromList . map (bimap wrap wrap) . HM.toList
+          wrap  = TE.encodeUtf8 . T.cons '%' . TE.decodeUtf8Lenient
+instance FromNamedRecord a => FromNamedRecord (Wrap a) where
+  parseNamedRecord  = fmap Wrap . parseNamedRecord . pre
+    where pre       = HM.fromList . map (bimap unwrap unwrap) . HM.toList
+          unwrap    = TE.encodeUtf8 . T.tail . TE.decodeUtf8Lenient
+instance DefaultOrdered a => DefaultOrdered (Wrap a) where
+  headerOrder   = fmap wrap . headerOrder . unWrap
+    where wrap  = TE.encodeUtf8 . T.cons '%' . TE.decodeUtf8Lenient
+
+-- |  Store a list of (CSV-transformable) data to a XLSX file.
+--    CAVEAT: round-trips don't work with unsafe text values.
+--    Use "LiBro.Data.SafeText".
+storeAsXlsx :: (DefaultOrdered a, ToNamedRecord a) => FilePath -> [a] -> IO ()
+storeAsXlsx fp records = do
+  withSystemTempDirectory "xlsx-export" $ \tdir -> do
+    let csvFile = tdir </> "export.csv"
+    LBS.writeFile csvFile $ encodeDefaultOrderedByName (Wrap <$> records)
+    callCommand $ unwords
+      [ "libreoffice --calc --convert-to xlsx"
+      , "--outdir", tdir, csvFile
+      , "> /dev/null"
+      ]
+    renameFile (csvFile -<.> "xlsx") fp
+
+-- |  Load a list of (CSV-transformable) data from a XLSX file.
+--    CAVEAT: round-trips don't work with unsafe text values.
+--    Use "LiBro.Data.SafeText".
+loadFromXlsx :: FromNamedRecord a => FilePath -> IO (Either String [a])
+loadFromXlsx fp = do
+  withSystemTempDirectory "xlsx-import" $ \tdir -> do
+    let xlsxFile = tdir </> "import.xlsx"
+    copyFile fp xlsxFile
+    callCommand $ unwords
+      [ "libreoffice --calc --convert-to csv"
+      , "--outdir", tdir, xlsxFile
+      , "> /dev/null"
+      ]
+    csv <- LBS.readFile (xlsxFile -<.> "csv")
+    return $ map unWrap . V.toList . snd <$> decodeByName csv
--- test/LiBro/UtilSpec.hs
+++ test/LiBro/UtilSpec.hs
@@ -3,16 +3,23 @@ module LiBro.UtilSpec where
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
+import Test.QuickCheck.Arbitrary.Generic
 
 import LiBro.Util
+import LiBro.Data.SafeText
 import Data.Tree
 import Data.Functor.Identity
+import Data.Csv
 import Control.Monad
+import GHC.Generics
+import System.FilePath
+import System.IO.Temp
 
 spec :: Spec
 spec = describe "Helper stuff" $ do
   forestFromParentList
   countingT
+  xlsx
   guarding
 
 forestFromParentList :: Spec
@@ -35,6 +42,40 @@ countingT = describe "The CountingT 'monad transformer'" $ do
     prop "Grab the following Ints" $ \(start, len) -> do
       let results = runCountingT (nextTimes len) start
       runIdentity results `shouldBe` take len [start..]
+
+data ExampleData = ED {foo :: Int, bar :: SafeText}
+  deriving (Eq, Show, Generic)
+instance FromNamedRecord ExampleData
+instance ToNamedRecord ExampleData
+instance DefaultOrdered ExampleData
+instance Arbitrary ExampleData where arbitrary = genericArbitrary
+
+xlsx :: Spec
+xlsx = describe "XLSX data conversion" $ do
+
+  describe "Reading from simple spreadsheet file" $ do
+    result <- runIO $ loadFromXlsx "test/storage-files/wrapped.xlsx"
+    it "Correct result" $
+      result `shouldBe` Right [ED 17 "42"]
+
+  describe "Load . store = id" $ do
+
+    context "With simple example data" $ do
+      let input = [ED 42 "Hallö\nWelt!", ED 0 "=foo"]
+      output <- runIO $ withSystemTempDirectory "xlsx-export" $ \tdir -> do
+        let fp = tdir </> "simple.xlsx"
+        storeAsXlsx fp input
+        loadFromXlsx fp
+      it "Correct output data" $ output `shouldBe` Right input
+
+    context "With arbitrary data" $ do
+      modifyMaxSuccess (const 5) $
+        prop "Correct output data" $ \d ->
+          ioProperty $ withSystemTempDirectory "xlsx-export" $ \tdir -> do
+            let fp = tdir </> "arbitrary.xlsx"
+            storeAsXlsx fp d
+            result <- loadFromXlsx fp
+            return $ result `shouldBe` Right (d :: [ExampleData])
 
 guarding :: Spec
 guarding = describe "Guarded Alternative value creation" $ do
