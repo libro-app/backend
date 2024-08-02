150a17309b97dfe59535426815fd786ec50ff43a
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2024-08-02 17:57:26 +0200

    Add forgotten haddocs for LiBro the monad

--- lib/LiBro/Base.hs
+++ lib/LiBro/Base.hs
@@ -4,6 +4,7 @@ module LiBro.Base where
 import LiBro.Config
 import Control.Monad.Reader
 
+-- |  Internal monad for 'Config'ured libro effects.
 newtype LiBro a = LiBro
   { unLiBro :: ReaderT Config IO a
   } deriving  ( Functor
@@ -14,5 +15,6 @@ newtype LiBro a = LiBro
               , MonadIO
               )
 
+-- |  Run a 'Config'ured libro effect in 'IO'.
 runLiBro :: Config -> LiBro a -> IO a
 runLiBro config = flip runReaderT config . unLiBro
